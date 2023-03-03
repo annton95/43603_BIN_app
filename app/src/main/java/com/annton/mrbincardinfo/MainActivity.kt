26245d43ba57d20a96c96c8ec2c159dd92f04dc1
annton95 <annton@live.ru>
2023-03-03 11:44:21 +0300

    update 3

--- app/src/main/java/com/annton/mrbincardinfo/MainActivity.kt
+++ app/src/main/java/com/annton/mrbincardinfo/MainActivity.kt
@@ -7,139 +7,94 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.content.edit
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.annton.mrbincardinfo.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: ArrayAdapter<String>
     private lateinit var sharedPreferences: SharedPreferences
     private var localResults: Boolean = false
     private var items = mutableListOf<String?>(null)
+    private var itemsDescription = mutableListOf<String?>(null)
     private var url = ""
+    private var searchUrl = ""
     private var phone = ""
     private var countryCoordinates = ""
     private var requestNumber = 0
     private lateinit var localCardInfo: CardInfo
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
-
         requestNumber = sharedPreferences.getInt("requestNumber", 0)
-
-
         updateListView()
-
         setUpOnClickForTextViews()
+        hideProgressBarAndResultsCard()
+        setUpOnClickButtons()
 
-        binding.cardViewCardResult.visibility = View.GONE
+    }
 
-        binding.progressBarCheck.visibility = View.GONE
-
-        binding.listView.setOnItemClickListener { _, _, position, _ ->
-            val item = adapter.getItem(position)
-            binding.editTextBin.setText(item)
-//            getSavedData(item)
-            localResults = true
-            getBinInfo(item.toString())
-        }
-
-
+    private fun setUpOnClickButtons() {
         binding.buttonClearList.setOnClickListener {
             sharedPreferences.edit {
                 clear()
                 apply()
             }
             items.clear()
-            adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-            binding.listView.adapter = adapter
+            itemsDescription.clear()
+            binding.cardViewPrevious.visibility = View.GONE
             requestNumber = 0
-
         }
-
         binding.buttonGetInfo.setOnClickListener {
             val bin = binding.editTextBin.text.toString().trim()
             localResults = false
+            binding.cardViewPrevious.visibility = View.VISIBLE
             getBinInfo(bin)
-
-
+            showResults()
         }
     }
 
-    private fun getBinInfo(bin: String) {
 
+    private fun getBinInfo(bin: String) {
         val client = BinListApiClient()
 
         if (localResults) {
             localCardInfo = getSavedCardInfo(bin)
         }
-
         if (bin.length <= 5) {
             Toast.makeText(this, "Please enter at least 6 symbols", Toast.LENGTH_SHORT).show()
-
         } else {
 
             hideResults()
-
 
             client.getCardInfo(bin, object : Callback<CardInfo> {
                 override fun onResponse(call: Call<CardInfo>, response: Response<CardInfo>) {
 
                     if (response.isSuccessful) {
-
                         requestNumber++
                         saveCardNumber(bin, response.body())
-                        if (!localResults) {
-                            updateListView()
-                        }
+                        if (!localResults) { updateListView() }
 
-
-                        var cardInfo: CardInfo? = null
-                        if (localResults) {
-                            cardInfo = localCardInfo
-                        } else {
-                            cardInfo = response.body()
-                        }
-
-//                        if(localResults){ getSavedCardInfo(bin)}
-//                        else{ cardInfo = response.body()}
-
+                        val cardInfo: CardInfo? = if (localResults) { localCardInfo }
+                        else { response.body() }
 
                         if (cardInfo != null) {
                             showResults()
-
-
-
-                            if (cardInfo.bank.name != null) {
-
-                                setUpUrls(cardInfo)
-
-                                setBankTextViews(cardInfo)
-
-
-                            } else {
-                                binding.textViewBankName.text = "no bank info"
-                                binding.textViewBankPhone.text = ""
-                                binding.textViewBankUrl.text = ""
-                            }
-
+                            setUpUrls(cardInfo)
+                            setBankTextViews(cardInfo)
                             setCardInfoTextViews(cardInfo)
-
 
                         } else {
                             hideResultsAndProgress()
-
                             Toast.makeText(
                                 this@MainActivity, "Card info not found", Toast.LENGTH_SHORT
                             ).show()
@@ -178,24 +133,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setCardInfoTextViews(cardInfo: CardInfo) {
-        binding.textViewCardType.text = "Type: ${cardInfo.type}"
-        binding.textViewCardBrand.text = "Brand: ${cardInfo.brand}"
-        binding.textViewCountry.text = "Country:${cardInfo.country.emoji}${cardInfo.country.name}"
-        binding.textViewPaymentSystem.text = "Scheme: ${cardInfo.scheme}"
-
-
-    }
+        binding.textViewCardType.text = cardInfo.type
+        binding.textViewCardBrand.text = cardInfo.brand
+        binding.textViewCountry.text = "${cardInfo.country.emoji}${cardInfo.country.name}"
+        binding.textViewPaymentSystem.text = cardInfo.scheme
+        }
 
     private fun setBankTextViews(cardInfo: CardInfo) {
-        binding.textViewBankName.text = "Bank: ${cardInfo.bank.name} City: ${cardInfo.bank.city}"
-        binding.textViewBankPhone.text = "tel.:\uD83D\uDCDE ${cardInfo.bank.phone} "
-        binding.textViewBankUrl.text = "url.:\uD83C\uDF0E ${cardInfo.bank.url} "
-
-
+        binding.textViewBankName.text = cardInfo.bank.name
+        binding.textViewCity.text = cardInfo.bank.city
+        binding.textViewBankPhone.text = "\uD83D\uDCDE ${cardInfo.bank.phone} "
+        binding.textViewBankUrl.text = "\uD83C\uDF0E ${cardInfo.bank.url} "
     }
 
     private fun hideResultsAndProgress() {
         hideResults()
+        binding.progressBarCheck.visibility = View.GONE
+    }
+
+    private fun hideProgressBarAndResultsCard() {
+        binding.cardViewCardResult.visibility = View.GONE
         binding.progressBarCheck.visibility = View.GONE
     }
 
@@ -214,7 +171,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpUrls(cardInfo: CardInfo) {
+        searchUrl = "https://www.google.com/search?q=${cardInfo.bank.name}"
         url = "https://${cardInfo.bank.url}"
+        if (cardInfo.bank.url == "") {
+            url = searchUrl
+        }
         phone = "tel:${cardInfo.bank.phone}"
         countryCoordinates = "geo:${cardInfo.country.latitude},${cardInfo.country.longitude}"
     }
@@ -231,7 +192,7 @@ class MainActivity : AppCompatActivity() {
         binding.textViewBankPhone.setOnClickListener {
 
             val intent = Intent(Intent.ACTION_DIAL).apply {
-                data = Uri.parse("$phone")
+                data = Uri.parse(phone)
             }
             if (intent.resolveActivity(packageManager) != null) {
                 startActivity(intent)
@@ -246,30 +207,52 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
-        }
+            binding.textViewBankName.setOnClickListener {
+                Intent(Intent.ACTION_VIEW, Uri.parse(searchUrl)).apply { startActivity(this) }
+            }
 
+        }
 
     }
 
-
     private fun updateListView() {
-        if (items != null) {
-            items.clear()
+        items.clear()
+        itemsDescription.clear()
 
+        for (i in 1..requestNumber) {
+            val item = sharedPreferences.getString("binRequest#$i", null)
 
-            for (i in 1..requestNumber) {
-                var item = sharedPreferences.getString("binRequest#$i", null)
+            val currentBin = item
 
-                items.add(item)
-            }
+            val itemDescription = "${
+                sharedPreferences.getString(
+                    "card#$currentBin bankName",
+                    "-"
+                )
+            }," + " ${
+                sharedPreferences.getString(
+                    "card#$currentBin country",
+                    ""
+                )
+            } " + "${sharedPreferences.getString("card#$currentBin country emoji", "")}"
 
+            items.add(item)
+            itemsDescription.add(itemDescription)
 
-            adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-
-            binding.listView.adapter = adapter
-        } else {
         }
 
+        val itemDescribed =
+            items.zip(itemsDescription).map { ListItem(it.first ?: "", it.second ?: "") }
+
+
+        binding.listView.layoutManager = LinearLayoutManager(this)
+        binding.listView.adapter = MyAdapter(itemDescribed) { item ->
+
+            binding.editTextBin.setText(item.title)
+            localResults = true
+            getBinInfo(item.title)
+
+        }
 
     }
 
@@ -308,7 +291,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         val bankSaved = CardBankInfo(
-            sharedPreferences.getString("card#$bin bankName", "no bank info").toString(),
+            sharedPreferences.getString("card#$bin bankName", getString(R.string.no_bank_info)).toString(),
             sharedPreferences.getString("card#$bin bankUrl", "").toString(),
             sharedPreferences.getString("card#$bin bankPhone", "").toString(),
             sharedPreferences.getString("card#$bin bankCity", "").toString()
@@ -324,7 +307,6 @@ class MainActivity : AppCompatActivity() {
             bankSaved
         )
     }
-
 
 }
 
